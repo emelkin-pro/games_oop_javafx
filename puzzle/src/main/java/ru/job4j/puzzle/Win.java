85bcc557145e9722a8f491ad250e0e1b3579aa1f
emelkin-pro <emelkin-pro@yandex.ru>
2021-08-20 07:05:58 +0300

    There is second solve.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,38 +2,33 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        return checkVertical(board) || checkHorizontal(board);
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (checkHorizontal(board, i) || checkVertical(board, i))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
-    public static boolean checkVertical(int[][] board) {
-        boolean verticalRsl = false;
-        for (int j = 0; j < board[0].length; j++) {
-            if (board[0][j] == 1) {
-                for (int i = 1; i < board.length; i++) {
-                    if (board[i][j] == 1) {
-                        verticalRsl = true;
-                    } else {
-                        verticalRsl = false;
-                        break;
-                    }
-                }
+    public static boolean checkVertical(int[][] board, int column) {
+        boolean verticalRsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 1) {
+                verticalRsl = false;
+                break;
             }
         }
         return verticalRsl;
     }
 
-    public static boolean checkHorizontal(int[][] board) {
+    public static boolean checkHorizontal(int[][] board, int row) {
         boolean horizontalRsl = true;
-        for (int i = 0; i < board[0].length; i++) {
-            if (board[i][0] == 1) {
-                for (int j = 1; j < board.length; j++) {
-                    if (board[i][j] == 1) {
-                        horizontalRsl = true;
-                    } else {
-                        horizontalRsl = false;
-                        break;
-                    }
-                }
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                horizontalRsl = false;
+                break;
             }
         }
         return horizontalRsl;
