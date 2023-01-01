5996ed49e579febc14db314da7916513992a0920
emelkin-pro <emelkin-pro@yandex.ru>
2023-01-01 11:56:05 +0300

    2. Каркас шахматной доски | реализация методов BishopBlack

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -2,6 +2,7 @@ package ru.job4j.chess;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+
 import java.util.Arrays;
 
 public final class Logic {
@@ -21,6 +22,14 @@ public final class Logic {
     }
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
+        for (Cell step : steps) {
+            try {
+                findBy(step);
+            } catch (FigureNotFoundException e) {
+                break;
+            }
+            throw new OccupiedCellException();
+        }
         return true;
     }
 
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,7 +1,11 @@
 package ru.job4j.chess;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
+
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
@@ -14,5 +18,34 @@ public class LogicTest {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testMoveImpossibleMoveExceptionC1toE4() throws Exception {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        thrown.expect(ImpossibleMoveException.class);
+        thrown.expectMessage("Could not move by diagonal from C1 to E4");
+        logic.move(Cell.C1, Cell.E4);
+    }
+
+    @Test
+    public void testMoveFigureNotFoundException() throws Exception {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        thrown.expect(FigureNotFoundException.class);
+        logic.move(Cell.A1, Cell.E4);
+    }
+
+    @Test
+    public void testMoveOccupiedCellException() throws Exception {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new BishopBlack(Cell.D2));
+        thrown.expect(OccupiedCellException.class);
+        logic.move(Cell.C1, Cell.E3);
     }
 }
